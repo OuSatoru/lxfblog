dd89a5bc2e29386a6d3b507941d816b99cafb074
Wang Cong <ousatoru@hotmail.com>
2016-05-22 22:38:11 +0800

    day 3 line 137 https://github.com/moling3650/mblog/blob/master/www/app/frame/orm.py line 107

---
+++ www/orm.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import asyncio, logging
+import aiomysql
+from .fields import Field
+
+__pool = object
+
+
+def log(sql, args=None):
+    logging.info('SQL: %s args: %s' % (sql, str(args or [])))
+
+
+async def create_pool(loop, **kwargs):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kwargs.get('host', 'localhost'),
+        port=kwargs.get('port', 3306),
+        user=kwargs['user'],
+        password=kwargs['password'],
+        db=kwargs['db'],
+        charset=kwargs.get('charset', 'utf8'),
+        autocommit=kwargs.get('autocommit', True),
+        maxsize=kwargs.get('maxsize', 10),
+        minsize=kwargs.get('minsize', 1),
+        loop=loop
+    )
+
+
+async def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+
+async def execute(sql, args, autocommit=True):
+    log(sql)
+    global __pool
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
+            raise e
+        return affected
+
+
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+
+class ModelMetaClass(type):
+    def __new__(cls, name, bases, attrs):
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+        tablename = attrs.get('__table__', None) or name
+        logging.info('found model %s (table %s)' % (name, tablename))
+        mappings = dict()
+        fields = []
+        primarykey = None
+        for k,v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    if primarykey:
+                        raise KeyError('Duplicate primary key for field: %s' % k)
+                    primarykey = k
+                else:
+                    fields.append(k)
+        if not primarykey:
+            raise KeyError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = tablename
+        attrs['__primary_key__'] = primarykey
+        attrs['__fields__'] = fields
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primarykey, ', '.join(escaped_fields), tablename)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
+        tablename, ', '.join(escaped_fields), primarykey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
+        tablename, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primarykey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tablename, primarykey)
+        return type.__new__(cls, name, bases, attrs)
